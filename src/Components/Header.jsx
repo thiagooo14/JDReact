de725137e6d375a37a3f85873ede072978901ca8
thiagooo14 <thiagooo15@gmail.com>
2021-05-26 21:06:32 -0300

    header +css

---
+++ src/Components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { badge } from '../assets/vectors';
+import './css/header.css';
+
+function Header() {
+  return (
+    <header>
+      <div className='left-header'>
+        <img src={badge} alt='logo' />
+        <nav className='btns'>
+          <ul className='nav-list'>
+            <li><a href="/"> Create Your Nanny Share</a></li>
+            <li><a href="/">Browse Shares</a></li>
+            <li><a href="/">Our Story</a></li>
+          </ul>
+        </nav>
+      </div>
+      <div className='rigth-header'>
+        <button className='btn-share-host'>Become a Nanny Share Host</button>
+        <a href="/" className='btn-sing-in'> Sing In </a>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
