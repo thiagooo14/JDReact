33dda5d09b02505d59665ed17ffd37647dc365aa
thiagooo14 <thiagooo15@gmail.com>
2021-05-31 19:49:15 -0300

    test A/B done

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import './scss/Banner.scss';
 import banner from '../assets/images/ImageHeader.png';
 import { play } from '../assets/vectors';
 
 function Banner() {
+  const [ABTest, setABTest] = useState(
+    parseInt(localStorage.getItem('AB'), 10)
+  );
+
+  useEffect(() => {
+    if (Number.isNaN(ABTest)) {
+      const ABValue = Math.floor(Math.random() * 2);
+      localStorage.setItem('AB', ABValue);
+      setABTest(parseInt(localStorage.getItem('AB'), 10));
+    }
+  });
+
   return (
     <section className='banner'>
       <Header />
       <div className='banner-wrap'>
         <article>
-          {/* tentar fazer A/B mais tarde */}
-          <h1>Easily create or join a local nanny share with Hapu</h1>
+          <h1>
+            {ABTest
+              ? 'Easily create or join a local nanny share with Hapu'
+              : 'Create the childcare you need at a price you can afford'}
+          </h1>
           <p>
-            Hapu is Airbnb for nanny share. Share your home, nanny and costs and
-            create new flexible, affordable solutions in childcare.
+            {ABTest
+              ? 'Hapu is Airbnb for nanny share. Share your home, nanny and costs and create new flexible, affordable solutions in childcare.'
+              : 'Connect with other local families to share a nanny from as low as $10.00/hr each. Create your family profile today to get started.'}
           </p>
           <div>
             <img src={play} alt='' />
